27c33fda52ae544433365ac517aee6f493943d08
Zhiqu Zhang <zhzzhang@adit.mines.edu>
2016-10-04 14:58:15 -0600

    added all files initial commit

---
+++ src/experiment/BoardCell.java
@@ -0,0 +1,16 @@
+package experiment;
+
+public class BoardCell {
+	private int row;
+	private int column;
+	
+	public BoardCell(int row, int column) {
+		this.row = row;
+		this.column = column;
+	}
+<<<<<<< HEAD
+=======
+
+>>>>>>> fc3ce3217921b63ab50193df67db15de920d4117
+}
+
